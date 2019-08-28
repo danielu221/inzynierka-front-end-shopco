36613187f33c5f9fe0fe1163b5d63c6d991efe93
Mateusz <mateusz.danieluk@ge.com>
2019-08-28 20:51:06 +0200

    Send products request onnly once

--- src/app/modules/products-list/pages/products-list.page.ts
+++ src/app/modules/products-list/pages/products-list.page.ts
@@ -2,9 +2,16 @@ import { Component, OnInit } from '@angular/core';
 import { Product } from 'src/app/shared/interface/product.interface';
 import { Store, select } from '@ngrx/store';
 import { ProductsPageState } from 'src/app/core/store/product/product.reducer';
-import { LoadProducts, AddToList, RemoveFromList } from 'src/app/core/store/product/product.action';
+import {
+  LoadProducts,
+  AddToList,
+  RemoveFromList
+} from 'src/app/core/store/product/product.action';
 import { Observable } from 'rxjs';
-import { selectAllProducts, selectAllProductsInCart } from 'src/app/core/store/product/product.selectors';
+import {
+  selectAllProducts,
+  selectAllProductsInCart
+} from 'src/app/core/store/product/product.selectors';
 import { ProductState } from 'src/app/core/store/product/ProductState';
 
 @Component({
@@ -13,31 +20,36 @@ import { ProductState } from 'src/app/core/store/product/ProductState';
   styleUrls: ['./products-list.page.scss']
 })
 export class ProductsListPage implements OnInit {
-
   products$: Observable<any>;
   productsInCart$: Observable<any>;
 
   products: Product[];
 
-
-  constructor(private store: Store<ProductsPageState>) { 
+  constructor(private store: Store<ProductsPageState>) {
     this.products$ = store.pipe(select(selectAllProducts));
-    this.productsInCart$ = store.pipe(select(selectAllProductsInCart))
+    this.productsInCart$ = store.pipe(select(selectAllProductsInCart));
   }
 
   ngOnInit() {
-    this.store.dispatch(new LoadProducts());
+    this.products$.subscribe(products => {
+      if (products.length === 0) {
+        this.store.dispatch(new LoadProducts());
+      }
+    });
   }
 
-  addToList(product:ProductState){
-    this.store.dispatch(new AddToList({ productName: product.productName,
-      unitPrice: product.unitPrice,
-      picture: product.picture,
-      id: product.id}));
+  addToList(product: ProductState) {
+    this.store.dispatch(
+      new AddToList({
+        productName: product.productName,
+        unitPrice: product.unitPrice,
+        picture: product.picture,
+        id: product.id
+      })
+    );
   }
 
-  removeFromList(product:ProductState){
+  removeFromList(product: ProductState) {
     this.store.dispatch(new RemoveFromList(product));
   }
-
 }
